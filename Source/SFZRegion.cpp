9f96b3eb27a84ab781c79c3b3bf228bd0c9e6e02
Steve Folta <steve@folta.net>
2012-04-16 22:49:13 -0700

    Added SFZRegion.

---
+++ Source/SFZRegion.cpp
@@ -0,0 +1,15 @@
+#include "SFZRegion.h"
+#include <string.h>
+
+
+SFZRegion::SFZRegion()
+{
+	memset(this, 0, sizeof(*this));
+	hikey = 127;
+	hivel = 127;
+	pitch_keycenter = 60; 	// C4
+	volume = pan = 0.0;
+}
+
+
+
---
+++ Source/SFZRegion.h
@@ -0,0 +1,40 @@
+#ifndef SFZRegion_h
+#define SFZRegion_h
+
+class SFZSample;
+
+
+// SFZRegion is designed to be able to be bitwise-copied.
+
+class SFZRegion {
+	public:
+		enum Trigger {
+			attack, release, first, legato
+			};
+		enum LoopMode {
+			no_loop, one_shot, loop_continuous, loop_sustain
+			};
+
+
+		SFZRegion();
+
+		SFZSample* sample;
+		unsigned char lokey, hikey;
+		unsigned char lovel, hivel;
+		Trigger trigger;
+		unsigned long group, off_by;
+
+		unsigned long offset;
+		unsigned long end;
+		LoopMode loop_mode;
+		unsigned long loop_start, loop_end;
+		int transpose;
+		int tune;
+		int pitch_keycenter;
+
+		float volume, pan;
+	};
+
+
+#endif 	// !SFZRegion_h
+
